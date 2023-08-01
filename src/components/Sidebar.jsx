06d6c47de6e917c12e1b701f93e3e929e8d71974
tedijulianto <tedijulianto92@gmail.com>
2023-08-01 21:00:39 +0700

    add toggle menu & search

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,10 +11,11 @@ import { BsTicketPerforatedFill } from "react-icons/bs";
 import { RiSettings4Fill } from "react-icons/ri";
 import { IoLogOut } from "react-icons/io5";
 import { FaSearch } from "react-icons/fa";
-import { FaBars } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 import ReactStars from "react-rating-stars-component";
 import Button from "../components/Button";
+import { useState } from "react";
+import { useRef } from "react";
 
 const Sidebar = ({ children }) => {
   const Menus = [
@@ -111,25 +112,93 @@ const Sidebar = ({ children }) => {
     halfIcon: <i className="fa fa-star-half-alt" />,
   };
 
+  const [isOpen, setOpen] = useState(false);
+  const [search, setSearch] = useState(true);
+
+  const inputRef = useRef(null);
+
+  const handleClick = () => {
+    inputRef.current.focus();
+  };
+
   return (
     <div className="grid xl:grid-cols-12">
       <aside className="xl:col-span-2 xl:pt-6 xl:border-r xl:border-zinc-700 xl:h-screen">
-        <div className="flex justify-between xl:justify-center items-center p-4 xl:p-0 xl:pb-6">
-          <FaSearch className="xl:hidden" />
-          <h1 className="text-3xl text-center font-bold">Anime</h1>
-          <FaBars className="xl:hidden" />
-        </div>
-        {Menus.map((menu, index) => (
-          <NavLink
-            to={menu.path}
-            key={index}
-            activeclassname="active"
-            className="sidebar hover:text-yellow-300 active:text-yellow-400 gap-5 xl:flex xl:items-center p-2 hidden"
+        <div className="flex justify-between xl:justify-center items-center p-4 xl:p-0 xl:pb-6 z-20 relative bg-[#242424]">
+          <button
+            className="lg:hidden"
+            onClick={() => {
+              setSearch(!search);
+              handleClick();
+            }}
           >
-            <div className="2xl:ml-24 ml-14">{menu.icon}</div>
-            <div>{menu.name}</div>
-          </NavLink>
-        ))}
+            <FaSearch />
+          </button>
+          <h1 className="text-3xl text-center font-bold">Anime</h1>
+          <button
+            className="relative rounded-sm focus:outline-none xl:hidden"
+            onClick={() => setOpen(!isOpen)}
+          >
+            <div className="absolute block w-5 right-0">
+              <span
+                aria-hidden="true"
+                className={`block absolute h-0.5 w-5 bg-current transform transition duration-500 ease-in-out ${
+                  isOpen ? "rotate-45" : "-translate-y-1.5"
+                }`}
+              ></span>
+              <span
+                aria-hidden="true"
+                className={`block absolute  h-0.5 w-5 bg-current transform transition duration-500 ease-in-out ${
+                  isOpen && "opacity-0"
+                }`}
+              ></span>
+              <span
+                aria-hidden="true"
+                className={`block absolute  h-0.5 w-5 bg-current transform transition duration-500 ease-in-out ${
+                  isOpen ? "-rotate-45" : "translate-y-1.5"
+                }`}
+              ></span>
+            </div>
+          </button>
+        </div>
+
+        <div
+          className={`backdrop-brightness-50 w-screen h-[68px] absolute lg:static lg:transform-none lg:hidden z-10 flex justify-center items-center duration-500 delay-100 ease-in-out top-0 transition ${
+            search ? "translate-y-0" : "translate-y-full"
+          }`}
+        >
+          <form className="w-full text-center">
+            <input
+              ref={inputRef}
+              type="text"
+              placeholder="Search here..."
+              className="text-center rounded-xl focus:ring-2 focus:ring-yellow-400 focus:outline-none bg-gray-700 appearance-none w-3/4 text-sm leading-6 text-yellow-400 placeholder-yellow-400 py-2"
+            />
+          </form>
+        </div>
+
+        <ul
+          className={`absolute lg:static z-10 bg-[#242424] lg:transform-none w-screen lg:w-full border-t border-b lg:border-t-0 lg:border-b-0 duration-500 delay-100 ease-in-out right-0 transition ${
+            isOpen ? "translate-x-0" : "translate-x-full"
+          }`}
+        >
+          <div className="my-2 xl:my-0">
+            {Menus.map((menu, index) => (
+              <li key={index} className="p-2 xl:px-0">
+                <NavLink
+                  to={menu.path}
+                  activeclassname="active"
+                  className="sidebar hover:text-yellow-300 gap-5 flex items-center"
+                >
+                  <div className="2xl:ml-[105px] xl:ml-14 ml-[135px]">
+                    {menu.icon}
+                  </div>
+                  <div>{menu.name}</div>
+                </NavLink>
+              </li>
+            ))}
+          </div>
+        </ul>
       </aside>
 
       <main className="xl:col-span-8 xl:p-6 px-3 xl:h-screen">{children}</main>
@@ -151,9 +220,11 @@ const Sidebar = ({ children }) => {
           />
         </div>
 
-        <div className="uppercase font-semibold xl:py-2 pb-2">popular this week</div>
+        <div className="uppercase font-semibold xl:py-2 pb-2">
+          popular this week
+        </div>
 
-        <ul className="flex flex-col 2xl:gap-6 gap-4 pb-3">
+        <ul className="flex flex-col 2xl:gap-6 gap-4 pb-3 2xl:pb-6">
           {Anime.map((anime, index) => (
             <li key={index} className="flex items-center">
               <NavLink>
